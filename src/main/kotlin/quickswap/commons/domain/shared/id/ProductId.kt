c8ec321f708771a1cbf118d24d9c39f964624d66
최준혁 <cwnsgur13579@gmail.com>
2025-10-19 20:26:44 +0900

    jpa converter

--- src/main/kotlin/quickswap/commons/domain/shared/id/ProductId.kt
+++ src/main/kotlin/quickswap/commons/domain/shared/id/ProductId.kt
@@ -1,8 +1,11 @@
 package quickswap.commons.domain.shared.id
 
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
 import quickswap.commons.domain.shared.IdProvider
 import java.io.Serializable
 
-data class ProductId(val value: String) : Serializable {
+@Embeddable
+data class ProductId(@Column(name = "id") val value: String) : Serializable {
   constructor(provider: IdProvider) : this(provider.provide())
 }
